efcb183dcb54767ab7f2f4f3550b488dd6e1d8fc
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2022-05-18 22:09:26 -0300

    Inicio Desafio05

---
+++ desafio05/router/webRouter.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const webRouter = express.Router()
+
+webRouter.get('/', (req, res) => {
+    res.sendFile('index.html', { root: './views'})
+})
+
+module.exports = { webRouter }
+
---
+++ desafio05/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const { webRouter } = require('./router/webRouter.js')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.use(webRouter)
+
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`escuchando en el puerto ${server.address().port}`)
+})
