396c560945471171ebe47525ae590bb4c4fabff1
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2022-07-13 21:23:04 -0300

    Error al levantar proyecto

---
+++ desafio13/src/middlewares/passport.js
@@ -0,0 +1,54 @@
+import passport from 'passport'
+import { Strategy } from 'passport-local'
+
+import { obtenerUsuarioPorId } from '../persistencia/usuarios.js'
+
+import { registrarUsuario } from '../api/usuariosApi.js'
+import { autenticar } from '../api/authApi.js'
+
+//cargar estrategias
+passport.use('registro', new Strategy({
+    passReqToCallback: true,
+    // usernameField: 'email',
+    // passwordField: 'contrasenia',
+},
+    (req, username, password, done) => {
+        try {
+            const datosUsuario = req.body
+            const usuario = registrarUsuario(req.body)
+            done(null, usuario)
+            // done(null, usuario, info) // donde info es un objeto, opcional
+        } catch (error) {
+            done(error)
+            // done(error, null, info) // donde info es un objeto, opcional
+        }
+    }))
+
+passport.use('login', new Strategy(
+    (username, password, done) => {
+        try {
+            const usuario = autenticar(username, password)
+            done(null, usuario)
+        } catch (error) {
+            done(null, false)
+        }
+    }))
+
+export const passportMiddleware = passport.initialize()
+
+// opcional =====================================================
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    try {
+        const user = obtenerUsuarioPorId(id)
+        done(null, user)
+    } catch (error) {
+        done(error)
+    }
+})
+
+export const passportSessionHandler = passport.session()
